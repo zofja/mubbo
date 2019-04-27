c12290e0d0a348c49487131b84cf6972db03be44
Zofia Partyka <zofiapartyka1@gmail.com>
2019-04-27 15:30:31 +0200

    changes StartScreen size to Windows size

--- src/gui/StartScreen.java
+++ src/gui/StartScreen.java
@@ -1,21 +1,25 @@
 package gui;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 
+/**
+ * Class opening the start screen. An object of class StartScreen displays:
+ * - app logo,
+ * - start button.
+ */
 class StartScreen extends JFrame {
 
+    /**
+     * Instantiates Start screen.
+     * @param startPanel {@code JPanel} object with {@code Logo} and {@code StartPanel} components.
+     */
     StartScreen(JPanel startPanel) {
-
         super("MuBbo");
-        setSize(new Dimension(700, 700));
+        setSize(1000, 900);
+        setMinimumSize(new Dimension(700, 700));
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         add(startPanel);
         setLocationRelativeTo(null);
         setVisible(true);
